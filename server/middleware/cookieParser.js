74538f620719532351f88f1c6c6bcc7d2c6b9aab
mariomui <yi.kan.mui@gmail.com>
2018-12-07 21:14:52 -0800

    Implement CookieParser

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,6 +1,26 @@
-const parseCookies = (req, res, next) => {
-    // console.log(req, 'req');
-    var cookie = req.headers.cookie;
+var session = require('../models/session');
+var Promise = require('bluebird');
+
+const parseData = options => {
+  return _.reduce(options, (parsed, value, key) => {
+    parsed.string.push(`${key} = ?`);
+    parsed.values.push(value);
+    return parsed;
+  }, { string: [], values: [] });
 };
+
+const parseCookies = (req, res, next) => {
+  let cookie = req.headers.cookie || '';
+  let cookies = {};
+  if (cookie.length !== 0) {
+    let cookieJar = cookie.split('; ');
+    cookieJar.forEach((cookie) => {
+      let pair = cookie.split('=');
+      cookies[pair[0]] = pair[1];
+    });
+  }
+  Object.assign(req.cookies, cookies);
+  next();
+}
 
 module.exports = parseCookies;
