d81571ad5521724102e91f2df746553e1782c4c5
mariomui <yi.kan.mui@gmail.com>
2018-12-07 22:10:49 -0800

    Implement Create Session For Auth

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,11 +1,20 @@
 const models = require('../models');
 const Promise = require('bluebird');
+const parseCookies = require('./cookieParser');
 
 module.exports.createSession = (req, res, next) => {
-
-};
+  if (!req.get('Cookie')) {
+    return models.Sessions.create()
+      .then((result) => {
+        return models.Sessions.get({ id: result.insertId })
+      }).then((session) => {
+        req.session = { hash: session.hash };
+        res.cookies = { 'shortlyid': { value: session.hash } };
+        next();
+      });
+  }
+}
 
 /************************************************************/
 // Add additional authentication middleware functions below
 /************************************************************/
-
--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,13 +1,6 @@
 var session = require('../models/session');
 var Promise = require('bluebird');
 
-const parseData = options => {
-  return _.reduce(options, (parsed, value, key) => {
-    parsed.string.push(`${key} = ?`);
-    parsed.values.push(value);
-    return parsed;
-  }, { string: [], values: [] });
-};
 
 const parseCookies = (req, res, next) => {
   let cookie = req.headers.cookie || '';
@@ -21,6 +14,6 @@ const parseCookies = (req, res, next) => {
   }
   Object.assign(req.cookies, cookies);
   next();
-}
+};
 
 module.exports = parseCookies;
