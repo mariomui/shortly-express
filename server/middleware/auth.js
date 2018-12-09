304e12335593d6a02cecfc25a788f8627b5f534b
mariomui <yi.kan.mui@gmail.com>
2018-12-08 17:40:17 -0800

    Refactor createSession again

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,44 +1,61 @@
 const models = require('../models');
 const Promise = require('bluebird');
-// const parseCookies = require('./cookieParser');
 
 module.exports.createSession = (req, res, next) => {
-  // console.log(req, 'whatthehell');
-  var flag = 'invalid';
-  console.log(req, 'the request obj');
-  if (req.cookies && (Object.keys(req.cookies).length > 0) && flag === 'invalid') {
-    console.log('first stage', req, 'first stage');
-    return models.Sessions.get({ hash: req.cookies.shortlyid })
-      .then((rowSession) => {
-        console.log(rowSession, 'rowsessh');
-        flag = 'valid';
-        console.log('reqthis', req);
-        if (rowSession.hash === req.cookies.shortlyid) {
-          req.session = { hash: rowSession.hash };
-          res.cookies = { 'shortlyid': { value: rowSession.hash } }
-        } else {
-          return models.Sessions.create().then((item) => {
 
-            next();
-          })
-        }
-      })
-      .then(() => {
-        next();
-      });
-  } else {
-    return models.Sessions.create()
-      .then((result) => {
-        return models.Sessions.get({ id: result.insertId })
-      }).then((session) => {
-        console.log(session, 'session!!!!')
-        req.session = { hash: session.hash };
-        res.cookies = { 'shortlyid': { value: session.hash } };
-        next();
-        // flag = 'invalid';
-      });
-  }
+  return models.Sessions.get({ hash: req.cookies.shortlyid })
+    .then((sessionRow) => {
+      if (sessionRow === undefined) {
+        throw new Error('Invalid cookie');
+      }
+      req.session = sessionRow;
+      next();
+    })
+    .catch((err) => {
+      models.Sessions.create()
+        .then(session => {
+          return models.Sessions.get({ id: session.insertId });
+        })
+        .then(sessionRow => {
+          req.session = { hash: sessionRow.hash };
+          res.cookie('shortlyid', sessionRow.hash);
+          next();
+        });
+    });
+
 }
+  // var flag = 'invalid';
+  // if (req.cookies && (Object.keys(req.cookies).length > 0) && flag === 'invalid') {
+  //   console.log('first stage', req, 'first stage');
+  //   return models.Sessions.get({ hash: req.cookies.shortlyid })
+  //     .then((rowSession) => {
+  //       console.log(rowSession, 'rowsessh');
+  //       flag = 'valid';
+  //       console.log('reqthis', req);
+  //       if (rowSession.hash === req.cookies.shortlyid) {
+  //         req.session = { hash: rowSession.hash };
+  //         res.cookies = { 'shortlyid': { value: rowSession.hash } }
+  //       } else {
+  //         return models.Sessions.create().then((item) => {
+  //           next();
+  //         });
+  //       }
+  //     })
+  //     .then(() => {
+  //       next();
+  //     });
+  // } else {
+  //   return models.Sessions.create()
+  //     .then((result) => {
+  //       return models.Sessions.get({ id: result.insertId })
+  //     }).then((session) => {
+  //       console.log(session, 'session!!!!')
+  //       req.session = { hash: session.hash };
+  //       res.cookies = { 'shortlyid': { value: session.hash } };
+  //       next();
+  //     });
+  // }
+// }
 
 /************************************************************/
 // Add additional authentication middleware functions below
